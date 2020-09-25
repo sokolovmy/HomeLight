bc8c9cff25bcba7d099d41f9ee7889d9f87e5746
Maxim Sokolov <sokolovmy@gmail.com>
2020-09-25 14:19:19 +0400

    Update cons.h

--- KEIL/holse/cons.h
+++ KEIL/holse/cons.h
@@ -10,6 +10,16 @@
 #define TERM_TIMEOUT 120 //Timeout in seconds about :)
 #define MAX_WORD_LENGTH 6 + 1
 
+#define _CSI_		"\033["
+#define _RESET_	_CSI_"0m"
+#define _TOGGLE_ _CSI_"33mToggle"_RESET_
+#define _ON_ _CSI_"32mOn"_RESET_
+#define _OFF_ _CSI_"31mOff"_RESET_
+
+#define _TTOGGLE_ _CSI_"33mTimer Toggle"_RESET_
+#define _TON_ _CSI_"32mTimer On"_RESET_
+#define _TOFF_ _CSI_"31mTimer Off"_RESET_
+
 //char * consBuf = 0;
 void consInit(void);
 void putBufPtr(uint8_t * Buf, uint32_t * Len);
